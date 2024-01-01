fb305e6ef0f681a38744fcf1a3ab6ef7eaabfd04
Tamilarasan Murugan <tamilmurugan2602@gmail.com>
2024-01-01 21:02:47 +0530

    shadow

---
+++ code.java
@@ -0,0 +1,14 @@
+class tamil
+{       
+        int i=10;
+        static int j=100;
+	public static void main(String[] args)
+	{
+           tamil ok = new tamil();
+           
+	   System.out.println(ok.i); 
+           System.out.println(tamil.j);
+           System.out.println(j);
+           System.out.println(ok.j);	
+        }
+} 
